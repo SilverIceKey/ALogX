05a954f5a16511e76f8cc3970127de73a2c18a4c
SilverIceKey <z516798599@gmail.com>
2025-11-19 14:39:33 +0800

    feat: 添加 Jitpack 发布配置

--- alogx/build.gradle.kts
+++ alogx/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("maven-publish")
 }
 
 android {
@@ -32,6 +33,18 @@ android {
     }
 }
 
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            afterEvaluate {
+                from(components["release"])
+                groupId = project.findProperty("GROUP_ID") as String // 使用 GROUP_ID 属性
+                artifactId = "ALogX"
+                version = project.findProperty("VERSION") as String // 使用 VERSION_NAME 属性
+            }
+        }
+    }
+}
 dependencies {
 
     implementation(libs.androidx.core.ktx)
